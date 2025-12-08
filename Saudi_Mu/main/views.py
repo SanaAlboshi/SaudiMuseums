25d7576a4d91b7a44b912b1b685d1d0d284fc766
Sana <sanaalboshi77@gmail.com>
2025-12-08 08:59:02 +0300

    updateuu

--- Saudi_Mu/main/views.py
+++ Saudi_Mu/main/views.py
@@ -5,8 +5,10 @@ from museum.models import Museum
 
 
 def home_view(request):
+    # آخر هيئتين
     authorities = Authority.objects.all().order_by('-id')[:3]
 
+    # آخر 6 متاحف
     museums = Museum.objects.all().order_by('-id')[:3]
 
     return render(request, 'main/home.html', {
